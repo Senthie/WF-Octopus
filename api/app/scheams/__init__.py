2f05f0559b6ba59f8f2692e3df6b5fe6ae7746e0
浪川 <1214391613@qq.com>
2026-04-21 12:12:24 +0800

    Add initial schema definitions for AI inspection records

---
+++ api/app/scheams/__init__.py
@@ -0,0 +1,15 @@
+"""
+Author: '浪川' '1214391613@qq.com'
+Date: 2026-04-20 17:44:34
+LastEditors: '浪川' '1214391613@qq.com'
+LastEditTime: 2026-04-20 17:45:02
+FilePath: /api/app/scheams/__init__.py
+Description: 导入前后端的数据模型
+
+
+Copyright (c) 2026 by '浪川' email: '1214391613@qq.com', All Rights Reserved.
+"""
+
+from app.scheams.ai_inspection_scheam import InspectionRecordin, InspectionRecordOut
+
+__all__ = ['InspectionRecordin', 'InspectionRecordOut']
