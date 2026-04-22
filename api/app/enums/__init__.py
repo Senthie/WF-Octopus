8356f4bc8b6d0b95d39625c5d51ba33056530674
浪川 <1214391613@qq.com>
2026-04-22 10:23:36 +0800

    fix(enums): Update __all__ to include CustomResponseCodeEnum and correct LastEditTime

--- api/app/enums/__init__.py
+++ api/app/enums/__init__.py
@@ -2,7 +2,7 @@
 Author: '浪川' '1214391613@qq.com'
 Date: 2026-04-21 15:38:24
 LastEditors: '浪川' '1214391613@qq.com'
-LastEditTime: 2026-04-21 15:43:00
+LastEditTime: 2026-04-21 17:04:05
 FilePath: /api/app/enums/__init__.py
 Description: 枚举类，用于定义巡检和任务的状态和结果。
 
@@ -11,6 +11,7 @@ Copyright (c) 2026 by '浪川' email: '1214391613@qq.com', All Rights Reserved.
 """
 
 from app.enums.inspection_enum import InspectionResultEnum
+from app.enums.response_code_enum import CustomResponseCodeEnum
 from app.enums.task_enum import TaskStatusEnum
 
-__all__ = ['InspectionResultEnum', 'TaskStatusEnum']
+__all__ = ['InspectionResultEnum', 'TaskStatusEnum', 'CustomResponseCodeEnum']
