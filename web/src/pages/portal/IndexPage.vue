b7f05ada14b4baa9544312bbdfed049bee2ac2bb
浪川 <1214391613@qq.com>
2026-05-06 11:32:00 +0800

    添加路由跳转

--- web/src/pages/portal/IndexPage.vue
+++ web/src/pages/portal/IndexPage.vue
@@ -1,14 +1,24 @@
-
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref } from "vue"
+import { useRouter } from "vue-router"
 
-
+const router = useRouter()
+const push_route = (path: string) => {
+  router.push(path)
+}
 </script>
 <template>
   <div>
     <div>
       <div class="row">
-        <div class="col-2"><q-btn color="white" text-color="black" label="巡检" /> </div>
+        <div class="col-2">
+          <q-btn
+            color="white"
+            text-color="black"
+            label="巡检"
+            @click="push_route('/inspection/')"
+          />
+        </div>
       </div>
     </div>
   </div>
