d5ef8f0bd6ce4af403ab6203bcbff381a3ae197e
浪川 <1214391613@qq.com>
2026-05-13 11:06:07 +0800

    添加head

--- api/main.py
+++ api/main.py
@@ -1,8 +1,19 @@
+"""
+Author: '浪川' '1214391613@qq.com'
+Date: 2026-04-02 17:07:58
+LastEditors: '浪川' '1214391613@qq.com'
+LastEditTime: 2026-05-13 11:05:57
+FilePath: /api/main.py
+Description: 创建FastAPI应用程序并定义根路由。
+
+Copyright (c) 2026 by '浪川' email: '1214391613@qq.com', All Rights Reserved.
+"""
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
 async def root():
-    return {"message": "Hello World"}
+    return {'message': 'Hello World'}
