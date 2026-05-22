deb68f47c27e10f56f93b0f1a2aae13d83e3f38f
浪川 <1214391613@qq.com>
2026-05-22 09:13:36 +0800

    添加点击头跳转到主页

--- web/src/layouts/InspectionLayout.vue
+++ web/src/layouts/InspectionLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="hHh lpR fFf">
     <q-header elevated class="bg-primary text-white" height-hint="98">
       <q-toolbar>
-        <q-toolbar-title>
+        <q-toolbar-title @click="$router.push('/main')">
           <q-avatar>
             <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
           </q-avatar>
